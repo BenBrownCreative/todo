8328c516b6d11536a2607da31791a5f2127aa852
Ben Brown <designwithbrown@gmail.com>
2020-07-26 16:10:38 -0500

    refactor

--- todos-functions.js
+++ todos-functions.js
@@ -1,9 +1,47 @@
 // read existing todos from local storage
 const getSavedToDos = function() {
     const notesJSON = localStorage.getItem('toDos');
-    if (notesJSON !== null) {
-        return JSON.parse(notesJSON);
-    } else {
-        return [];
-    }
+    return notesJSON !== null ? JSON.parse(notesJSON) : [];
 };
+
+// add to do to local storage
+const storeInput = function(input) {
+    toDos.push({
+        text: input.value,
+        completed: false
+    });
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+};
+
+// render to do to dom
+const addHTML = function(filteredTodos, incompleteTodos) {
+    // render summary
+    const summary = document.createElement('h2');
+    summary.textContent = `you have ${incompleteTodos.length} todos left to do`;
+    document.querySelector('#toDosList').appendChild(summary);
+
+    // render each to do
+    filteredTodos.forEach(function (todos) {
+        let newParagraph = document.createElement('p');
+        newParagraph.textContent = (todos.text);
+        document.querySelector('#toDosList').appendChild(newParagraph);
+    });
+}
+
+// 
+const renderTodos = function(todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+        return searchTextMatch && hideCompletedMatch;
+    });
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed;
+    });
+
+    document.querySelector('#toDosList').innerHTML = '';
+    addHTML(filteredTodos, incompleteTodos); 
+};
+
+
--- todos.js
+++ todos.js
@@ -5,33 +5,9 @@ const filters = {
     hideCompleted: false
 };
 
-let toDos = getSavedToDos();
+const toDos = getSavedToDos();
 
-const renderTodos = function(todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
 
-        return searchTextMatch && hideCompletedMatch;
-
-    });
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed;
-    });
-
-    document.querySelector('#toDosList').innerHTML = '';
-
-    const summary = document.createElement('h2');
-    summary.textContent = `you have ${incompleteTodos.length} todos left to do`;
-    document.querySelector('#toDosList').appendChild(summary);
-
-    filteredTodos.forEach(function (todos) {
-        let newParagraph = document.createElement('p');
-        newParagraph.textContent = (todos.text);
-        document.querySelector('#toDosList').appendChild(newParagraph);
-    });
-    
-}
 renderTodos(toDos, filters);
 
 document.querySelector('#newTodoFilter').addEventListener('input', function(e) {
@@ -42,14 +18,8 @@ document.querySelector('#newTodoFilter').addEventListener('input', function(e) {
 document.querySelector('#addToDoForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const input = e.target.elements.newTodoInput;
-    toDos.push({
-        text: input.value,
-        completed: false
-    });
-    localStorage.setItem('toDos', JSON.stringify(toDos));
-
+    storeInput(input);
     input.value = '';
-
     renderTodos(toDos, filters);
 });
 
