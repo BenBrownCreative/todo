2ce3ada913adde9fc546c5dc4149b389a5ef4ce6
Ben Brown <designwithbrown@gmail.com>
2020-09-23 11:08:49 -0500

    formatting

--- js/todos-functions.js
+++ js/todos-functions.js
@@ -1,69 +1,80 @@
+'use strict';
+
 // read existing todos from local storage
-const getSavedToDos = function() {
-    const notesJSON = localStorage.getItem('toDos');
+const getSavedToDos = function () {
+  const notesJSON = localStorage.getItem('toDos');
+
+  try {
     return notesJSON !== null ? JSON.parse(notesJSON) : [];
+  } catch (e) {
+    return [];
+  }
 };
 
 // add to do to local storage
-const storeInput = function(input) {
-    toDos.push({
-        id: uuidv4(),
-        text: input.value,
-        completed: false
-    });
-    localStorage.setItem('toDos', JSON.stringify(toDos));
+const storeInput = function (input) {
+  toDos.push({
+    id: uuidv4(),
+    text: input.value,
+    completed: false,
+  });
+  localStorage.setItem('toDos', JSON.stringify(toDos));
 };
 
-const saveAndRender = function(toDos) {
-    // save todos
-    localStorage.setItem('toDos', JSON.stringify(toDos));
-    // rerender
-    renderTodos(toDos, filters);
-}
+const saveAndRender = function (toDos) {
+  // save todos
+  localStorage.setItem('toDos', JSON.stringify(toDos));
+  // rerender
+  renderTodos(toDos, filters);
+};
 
-const createHTML = function(todos) {
-    const newParagraph = document.createElement('div');
-    newParagraph.className = 'listItem';
-    newParagraph.dataset.id = todos.id;
-    const markup = `
-        <i class="todoBoxWrapper material-icons md-light md-18 ${todos.completed ? 'completed' : ''}">done
-            <input type="checkbox" class="todoBox" ${todos.completed ? 'checked' : ''}/> 
+const createHTML = function (todos) {
+  const newParagraph = document.createElement('div');
+  newParagraph.className = 'listItem';
+  newParagraph.dataset.id = todos.id;
+  const markup = `
+        <i class="todoBoxWrapper material-icons md-light md-18 ${
+          todos.completed ? 'completed' : ''
+        }">done
+            <input type="checkbox" class="todoBox" ${
+              todos.completed ? 'checked' : ''
+            }/> 
         </i>
         <span>${todos.text}</span>
         <i class='remove material-icons md-light md-18'>remove_circle_outline</button>
     `;
-    newParagraph.innerHTML = markup;
-    return newParagraph;
-}
-
-// render to do to dom
-const addHTML = function(filteredTodos, incompleteTodos) {
-    // render summary
-    const summary = document.createElement('h2');
-    summary.textContent = `${incompleteTodos.length} todos left to do`;
-    document.querySelector('#toDosList').appendChild(summary);
-
-    // render each to do
-    filteredTodos.forEach(function (todos) {
-        let newItemHTML = createHTML(todos);
-        document.querySelector('#toDosList').appendChild(newItemHTML);
-    });
-}
-
-// 
-const renderTodos = function(todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-
-        return searchTextMatch && hideCompletedMatch;
-    });
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed;
-    });
-
-    document.querySelector('#toDosList').innerHTML = '';
-    addHTML(filteredTodos, incompleteTodos); 
+  newParagraph.innerHTML = markup;
+  return newParagraph;
 };
 
+// render to do to dom
+const addHTML = function (filteredTodos, incompleteTodos) {
+  // render summary
+  const summary = document.createElement('h2');
+  summary.textContent = `${incompleteTodos.length} todos left to do`;
+  document.querySelector('#toDosList').appendChild(summary);
 
+  // render each to do
+  filteredTodos.forEach(function (todos) {
+    let newItemHTML = createHTML(todos);
+    document.querySelector('#toDosList').appendChild(newItemHTML);
+  });
+};
+
+//
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filters.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+    return searchTextMatch && hideCompletedMatch;
+  });
+  const incompleteTodos = filteredTodos.filter(function (todo) {
+    return !todo.completed;
+  });
+
+  document.querySelector('#toDosList').innerHTML = '';
+  addHTML(filteredTodos, incompleteTodos);
+};
--- js/todos.js
+++ js/todos.js
@@ -1,45 +1,49 @@
+'use strict';
+
 const body = document.querySelector('body');
 
 const filters = {
-    searchText: '',
-    hideCompleted: false
+  searchText: '',
+  hideCompleted: false,
 };
 
 let toDos = getSavedToDos();
 
 renderTodos(toDos, filters);
 
-document.querySelector('#newTodoFilter').addEventListener('input', function(e) {
+document
+  .querySelector('#newTodoFilter')
+  .addEventListener('input', function (e) {
     filters.searchText = e.target.value;
     renderTodos(toDos, filters);
+  });
+
+document.querySelector('#addToDoForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const input = e.target.elements.newTodoInput;
+  storeInput(input);
+  input.value = '';
+  renderTodos(toDos, filters);
 });
 
-document.querySelector('#addToDoForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const input = e.target.elements.newTodoInput;
-    storeInput(input);
-    input.value = '';
-    renderTodos(toDos, filters);
-});
-
-document.querySelector('#hideCompleted').addEventListener('change', function(e) {
+document
+  .querySelector('#hideCompleted')
+  .addEventListener('change', function (e) {
     filters.hideCompleted = e.target.checked;
     renderTodos(toDos, filters);
-});
-document.getElementById('toDosList').addEventListener('click', function(e) {
-    // check to see if its the remove button
-    if (e.target.classList.contains('remove')) {
-        const elemId = e.target.parentElement.dataset.id;
-        // filter out the target by id 
-        toDos = toDos.filter(x => x.id !== elemId);
-        saveAndRender(toDos);
-    }
-    else if (e.target.classList.contains('todoBox')) {
-        const elemId = e.target.parentElement.parentElement.dataset.id;
-        let elem = toDos.find(x => x.id == elemId);
-        elem.completed = e.target.checked;
-        saveAndRender(toDos);
-    }
-    return
-    
+  });
+document.getElementById('toDosList').addEventListener('click', function (e) {
+  // check to see if its the remove button
+  if (e.target.classList.contains('remove')) {
+    const elemId = e.target.parentElement.dataset.id;
+    // filter out the target by id
+    toDos = toDos.filter((x) => x.id !== elemId);
+    saveAndRender(toDos);
+  } else if (e.target.classList.contains('todoBox')) {
+    const elemId = e.target.parentElement.parentElement.dataset.id;
+    let elem = toDos.find((x) => x.id == elemId);
+    elem.completed = e.target.checked;
+    saveAndRender(toDos);
+  }
+  return;
 });
